633f3001ff9921dbe14e39dddb42a2e0e150aa2b
Daniel Pérez <dani_mm__@MacBook-Air-de-Daniel.local>
2023-10-24 18:08:18 +0200

    borrada archivos printf

--- ft_bzero.c
+++
@@ -1,7 +0,0 @@
-
-#include "ft_printf.h"
-
-void	ft_bzero(void *s, size_t n)
-{
-	ft_memset(s, 0, n);
-}
--- ft_putchar.c
+++
@@ -1,9 +0,0 @@
-
-
-#include "ft_printf.h"
-
-int	ft_putchar(int c)
-{
-	write(1, &c, 1);
-	return (1);
-}
